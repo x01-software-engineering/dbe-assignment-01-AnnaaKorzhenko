feeb60622f45d56b5f57441deec10f2719f0ccbc
Anna Korzhenko <akorzhenko@kse.org.ua>
2024-04-09 22:25:35 +0300

    updated naming

--- pa4executions.sql
+++ pa4executions.sql
@@ -6,13 +6,13 @@ CALL get_team_info_sproc(@teamName);
 -- OUT
 -- it gives total count of existing players as an output
 SET @total_pLayers = NULL;
-CALL get_player_count_sproc(@total_players);
+CALL get_number_of_players_sproc(@total_players);
 SELECT @total_players;
 
 -- INOUT
 -- it takes player id as an IN parameter and then returns number of goals scored bt this player as an OUT parameter, but using INOUT
 SET @param = 3;
-CALL calculate_player_goals_sproc(@param);
+CALL calculate_players_goals_sproc(@param);
 SELECT @param;
 
 -- TRANSACTION
